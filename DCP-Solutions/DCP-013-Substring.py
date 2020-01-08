b8508fa6c86dc7a744dd1b6b7f80d4aadad14250
Andre Lima <50120293+drelima82@users.noreply.github.com>
2020-01-08 09:52:10 -0300

    fixing behavior
    - Rename get_longest_substring to get_longest_substring_andre1
    - Add new method "get_longest_substring_andre2" to have same behavior as get_longest_sub_with_k_dist

--- DCP-Solutions/DCP-013-Substring.py
+++ DCP-Solutions/DCP-013-Substring.py
@@ -1,17 +1,74 @@
 """
+Problem #13
+
 This problem was asked by Amazon.
 
 Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
 
 For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
 """
 
-def get_longest_substring(k, word):
+'''
+largest = ''
+
+abcba
+
+[position 0]
+a
+set([a])
+
+ab
+set([a b])
+
+abc
+set([a b c])
+
+> if len(set) > len(largest): largest = 'ab'
+
+
+[position 1]
+a
+set([a])
+
+
+qwertyuabccbccccbcba
+'''
+def get_longest_substring_andre2(word, k):
+    largest_string = ''
+    current_string = ''
+    current_k_map = []
+
+    index = 0
+    for current_letter in word:
+        current_k_map = []
+        current_string = ''
+        current_k_map.append(current_letter)
+        current_string += current_letter
+        for letter_for_substring in word[index+1:]:
+            if letter_for_substring not in current_k_map:
+                if len(current_k_map) < k:
+                    current_k_map.append(letter_for_substring)
+                    current_string += letter_for_substring
+                else:
+                    break
+            else:
+                current_string += letter_for_substring
+        
+        #Checking if current substring is larger than previous one
+        if len(largest_string) < len(current_string):
+            largest_string = current_string
+
+        index += 1
+
+    return largest_string
+
+def get_longest_substring_andre1(word, k):
     if k < 0 or k >= len(word):
         raise Exception("Invalid value for k:", k)
 
     element_at_k = word[k-1]
-    print("element_at_k:", element_at_k)
+    #print("element_at_k:", element_at_k)
     subs = word
 
     subs = element_at_k
@@ -64,10 +121,15 @@ def get_longest_sub_with_k_dist(s, k):
     return longest_substring
 
 
-assert get_longest_substring(2, "abcba") == "bcb"
-assert get_longest_substring(3, "abcbadcfebg") == "cbadc"
-assert get_longest_substring(5, "abcbafcarc") == "afca"
+assert get_longest_substring_andre1("abcba", 2) == "bcb"
+assert get_longest_substring_andre1("abcbadcfebg", 3) == "cbadc"
+assert get_longest_substring_andre1("abcbafcarc", 5) == "afca"
 
+assert get_longest_substring_andre2("abcba", 2) == "bcb"
+assert get_longest_substring_andre2("abccbba", 2) == "bccbb"
+assert get_longest_substring_andre2("abcbbbabbcbbadd", 2) == "bbbabb"
+assert get_longest_substring_andre2("abcbbbaaaaaaaaaabbcbbadd", 1) == "aaaaaaaaaa"
+assert get_longest_substring_andre2("abccbba", 3) == "abccbba"
 
 assert get_longest_sub_with_k_dist("abcba", 2) == "bcb"
 assert get_longest_sub_with_k_dist("abccbba", 2) == "bccbb"
